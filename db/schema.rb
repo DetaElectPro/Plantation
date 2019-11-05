20fb08d30340ca50062de8af33171426933093e1
Tayeb-Ali <Mr.elteb@gmail.com>
2019-11-05 21:23:42 +0200

    update schema add seeds demo data user and place

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_172051) do
+ActiveRecord::Schema.define(version: 2019_11_04_200734) do
 
   create_table "places", force: :cascade do |t|
     t.string "name"
-    t.string "deta"
+    t.string "description"
+    t.string "logo"
+    t.string "latitude"
+    t.string "longitude"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,37 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  movies = Place.create([{ name: 'Star Wars', deta: 'ome' }, { name: 'Lord of the Rings', deta: ' omg' }])
+# Add demo Users
+User.create([
+                {
+                    name: 'Elteyab Hassan',
+                    username: 'Admin',
+                    phone: '0122884466',
+                    password: 'admin12'
+                }, {name: 'Omer Ali',
+                    username: 'Owner',
+                    phone: '0114847667',
+                    password: '123456'
+                },])
+# Add demo Places
+Place.create([
+                 {name: 'Deta Group',
+                  description: 'DetaGroup Plants company',
+                  logo: 'default_logo.png',
+                  latitude: 32.33454,
+                  longitude: 15.45343,
+                  user_id: 1
+                 }, {name: 'Deta Group two',
+                     description: 'DetaGroup Plants company',
+                     logo: 'default_logo.png',
+                     latitude: 32.13454,
+                     longitude: 15.45343,
+                     user_id: 1
+                 }, {name: 'Deta Group three',
+                     description: 'DetaGroup Plants company',
+                     logo: 'default_logo.png',
+                     latitude: 32.63454,
+                     longitude: 15.75343,
+                     user_id: 2
+                 },])
 #   Character.create(name: 'Luke', movie: movies.first)
