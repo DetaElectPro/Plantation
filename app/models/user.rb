25d4b8671f211db6495430cf3c360e7176e0fd45
Tayeb-Ali <Mr.elteb@gmail.com>
2019-11-05 19:28:43 +0200

    update User model edit email to phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   mount_uploader :avatar, AvatarUploader
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, presence: true, uniqueness: true
+  # validates :phone, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
   validates :password,
             length: { minimum: 6 },
