97076f3b82fa817eb38b86cd551794408e0b74ba
Tayeb-Ali <Mr.elteb@gmail.com>
2019-11-05 19:26:30 +0200

    add user Controller creat& index with constrict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationApiController
+  before_action :authorize_request, except: :create
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 end
