f7cc74da4163fa8d08d3b4b107e18bff00ae7ab0
Tayeb-Ali <Mr.elteb@gmail.com>
2019-11-05 20:27:13 +0200

    update PlaceController add: index & find

--- app/controllers/articles_controller.rb
+++
@@ -1,7 +0,0 @@
-class ArticlesController < ApplicationController
-
-    def new
-        return 
-    end
-    
-end
--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,8 +1,12 @@
 class PlaceController < ApplicationController
   def index
+    @places = Place.all
+    render json: @places, status: :ok
   end
 
   def show
+    @id =params[:_id]
+    @place = Place.find_by(id: @id)
   end
 
   def new
