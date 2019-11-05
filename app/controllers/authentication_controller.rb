730f734a2f1df4c972dc1f62cb2d8db7088cc46a
Tayeb-Ali <Mr.elteb@gmail.com>
2019-11-05 19:35:57 +0200

    add Authentication Controller add: login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,16 @@
-class AuthenticationController < ApplicationController
+class AuthenticationController < ApplicationApiController
+
+  # POST /auth/login
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: {token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                    username: @user.username}, status: :ok
+    else
+      render json: {error: 'unauthorized'}, status: :unauthorized
+    end
+  end
+
 end
